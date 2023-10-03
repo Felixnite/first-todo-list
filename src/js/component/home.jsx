8810a98ebaf8aac7f9b2bb5f303be002ea746f6d
Felixnite <141184414+Felixnite@users.noreply.github.com>
2023-10-03 00:32:17 +0000

    filter part

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [todos, setTodos] = useState([])
+	
+	const deleteTask = (id)=>{
+		let newArray= todos.filter((item, index) => index !== id);
+    setTodos(newArray);
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+		  <h1>My To-Do List</h1>
+		  <ul>
+			<li>
+			  <input
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={(e) => {
+				  if (e.key === "Enter") {
+					setTodos(todos.concat(inputValue));
+					setInputValue("");
+				  }
+				}}
+				placeholder="Ingresa la tarea"
+			  />
+			</li>
+			{todos.map((item, index) => (
+			  <li key={index} onClick={() => deleteTask(index)}>
+				{item}
+			  </li>
+			))}
+		  </ul>
 		</div>
-	);
-};
+	  );
+	};
 
 export default Home;
