9dabc5ae380152dd80053e63368c8dd4499e2b49
Felixnite <141184414+Felixnite@users.noreply.github.com>
2023-10-08 04:10:53 +0000

    trash icon and counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,37 +7,52 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [inputValue, setInputValue] = useState("")
 	const [todos, setTodos] = useState([])
-	
-	const deleteTask = (id)=>{
-		let newArray= todos.filter((item, index) => index !== id);
-    setTodos(newArray);
+
+
+	const deleteTask = (id) => {
+		let newArray = todos.filter((item, index) => index !== id);
+		setTodos(newArray);
 	}
+
+
+
 	return (
 		<div className="container">
-		  <h1>My To-Do List</h1>
-		  <ul>
-			<li>
-			  <input
-				type="text"
-				onChange={(e) => setInputValue(e.target.value)}
-				value={inputValue}
-				onKeyDown={(e) => {
-				  if (e.key === "Enter") {
-					setTodos(todos.concat(inputValue));
-					setInputValue("");
-				  }
-				}}
-				placeholder="Ingresa la tarea"
-			  />
-			</li>
-			{todos.map((item, index) => (
-			  <li key={index} onClick={() => deleteTask(index)}>
-				{item}
-			  </li>
-			))}
-		  </ul>
+			<h1> My To-Do List </h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
+						}}
+						placeholder="Ingresa la tarea"
+					/>
+				</li>
+				{todos.map((item, index) => (
+					<li key={index} className="task-item">
+						<span>
+							{item}
+							<i className="fas fa-trash" onClick={() => deleteTask(index)}></i>
+							
+						</span>
+					</li>
+
+				))}
+
+
+			</ul>
+			<p>
+			{todos.length > 0 ? `There are ${todos.length} tasks` : 'There are no tasks'}
+			</p>
+
 		</div>
-	  );
-	};
+	);
+};
 
 export default Home;
